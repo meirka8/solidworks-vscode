138d261909b546653e447ed5881ad33ee15c26ab
meirka8 <67155085+meirka8@users.noreply.github.com>
2023-04-10 22:00:16 +0300

    working variables reorganization

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -1,4 +1,28 @@
 import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
-let client
+let client: LanguageClient;
+
+export function activate(context: vscode.ExtensionContext): void {
+  const serverModule = context.asAbsolutePath('server/out/server.js');
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'solidworks-equations' }],
+    synchronize: {
+      configurationSection: 'solidworksEquationsHighlighter',
+      fileEvents: vscode.workspace.createFileSystemWatcher('**/.eqn'),
+    },
+  };
+
+  client = new LanguageClient('solidworksEquationsHighlighter', 'SolidWorks Equations Highlighter', serverOptions, clientOptions);
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  return client ? client.stop() : undefined;
+}
