d935de67b7d778ddf2a8768f94fe199e2c55a277
meirka8 <67155085+meirka8@users.noreply.github.com>
2023-09-17 22:24:32 +0300

    quotation mark capture BROKEN

--- src/client/definition_provider.ts
+++ src/client/definition_provider.ts
@@ -12,21 +12,21 @@ interface VariableLocation {
     line: number;
     start: number;
     end: number;
-  }
-  
+}
+
 
 export function getVariables(document: vscode.TextDocument, regex: RegExp): VariableLocation[] {
     const text = document.getText();
     let match;
     const variables: VariableLocation[] = [];
-  
+
     while ((match = regex.exec(text)) !== null) {
         const line = document.positionAt(match.index).line;
         const start = document.positionAt(match.index).character;
-        
+
         // Use match[1] instead of match[0] to get only the captured variable name
-        const end = start + match[1].length; 
-  
+        const end = start + match[1].length;
+
         variables.push({
             name: match[1], // Use match[1] to get only the variable name
             line,
@@ -34,40 +34,53 @@ export function getVariables(document: vscode.TextDocument, regex: RegExp): Vari
             end
         });
     }
-  
+
     return variables;
-  }
-  
+}
+
 
 export class SolidworksEquationsDefinitionProvider implements vscode.DefinitionProvider {
     public async provideDefinition(
-      document: vscode.TextDocument,
-      position: vscode.Position,
-      token: vscode.CancellationToken
+        document: vscode.TextDocument,
+        position: vscode.Position,
+        token: vscode.CancellationToken
     ): Promise<vscode.Definition | undefined> {
-      const range = document.getWordRangeAtPosition(position);
-      if (!range) {
-        return undefined;
-      }
-  
-      const name = document.getText(range);
-  
-      // Here you can use your `getVariables` function or similar logic to find the definition
-      const definedVariableLocations = getVariables(document, varRegex); // Replace with actual regex
-  
-      for (const variable of definedVariableLocations) {
-        if (variable.name === name) {
-          return new vscode.Location(
-            document.uri,
-            new vscode.Range(
-              new vscode.Position(variable.line, variable.start),
-              new vscode.Position(variable.line, variable.end)
-            )
-          );
+        let range = document.getWordRangeAtPosition(position);
+        if (!range) {
+            return undefined;
         }
-      }
-  
-      return undefined;
+
+
+        // Expand the range to include surrounding quotation marks, if any
+        const startCharacter = range.start.character;
+        const endCharacter = range.end.character;
+
+        if (document.getText(new vscode.Range(new vscode.Position(range.start.line, startCharacter - 1), new vscode.Position(range.start.line, startCharacter))) === '"') {
+            range = new vscode.Range(new vscode.Position(range.start.line, startCharacter - 1), new vscode.Position(range.end.line, endCharacter));
+        }
+
+        if (document.getText(new vscode.Range(new vscode.Position(range.end.line, endCharacter), new vscode.Position(range.end.line, endCharacter + 1))) === '"') {
+            range = new vscode.Range(new vscode.Position(range.start.line, startCharacter), new vscode.Position(range.end.line, endCharacter + 1));
+        }
+
+
+        const name = document.getText(range);
+
+        // Here you can use your `getVariables` function or similar logic to find the definition
+        const definedVariableLocations = getVariables(document, varRegex); // Replace with actual regex
+
+        for (const variable of definedVariableLocations) {
+            if (variable.name === name) {
+                return new vscode.Location(
+                    document.uri,
+                    new vscode.Range(
+                        new vscode.Position(variable.line, variable.start),
+                        new vscode.Position(variable.line, variable.end)
+                    )
+                );
+            }
+        }
+
+        return undefined;
     }
-  }
-  
+}
