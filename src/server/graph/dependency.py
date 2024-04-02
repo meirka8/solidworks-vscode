d5f87aa7e61162dee25dcecde56f26378db335dd
Meir Kanevskiy <meir.kanevskiy@gmail.com>
2024-04-02 13:59:56 +0300

    Add VariableDependencyListener class to build dependency graph

---
+++ src/server/graph/dependency.py
@@ -0,0 +1,30 @@
+import networkx as nx
+from server.grammar.SolidWorksEquationsListener import SolidWorksEquationsListener
+from server.grammar.SolidWorksEquationsParser import SolidWorksEquationsParser
+
+
+class VariableDependencyListener(SolidWorksEquationsListener):
+    def __init__(self):
+        self.G = nx.DiGraph()
+        self.dependency_list = []
+
+    def enterVariableDefinition(
+        self, ctx: SolidWorksEquationsParser.VariableDefinitionContext
+    ):
+        variable = ctx.VARIABLE().getText()
+        self.dependency_list = []
+        self.G.add_node(
+            variable, type="variable", expression=ctx.expression().getText()
+        )
+
+    def enterExpression(self, ctx: SolidWorksEquationsParser.ExpressionContext):
+        if ctx.VARIABLE():
+            variable = ctx.VARIABLE().getText()
+            self.dependency_list.append(variable)
+
+    def exitVariableDefinition(
+        self, ctx: SolidWorksEquationsParser.VariableDefinitionContext
+    ):
+        variable = ctx.VARIABLE().getText()
+        for v in self.dependency_list:
+            self.G.add_edge(v, variable)
