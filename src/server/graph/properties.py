107452f20e64efc17f75fcd603c944c8c0b4f853
Meir Kanevskiy <meir.kanevskiy@gmail.com>
2024-04-02 21:34:51 +0300

    missing defintions veriables

--- src/server/graph/properties.py
+++ src/server/graph/properties.py
@@ -5,6 +5,10 @@ def basic_properties_check(G):
 
     is_dag = nx.is_directed_acyclic_graph(G)
 
+    # list of nodes without an 'expression' property
+    missing_expression = [node for node in G.nodes if "expression" not in G.nodes[node]]
+
     return {
         "is_dag": is_dag,
+        "missing_expression": missing_expression,
     }
