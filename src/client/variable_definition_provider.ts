29281d4c6504685c7f5e4681dbb42d010f273aac
meirka8 <67155085+meirka8@users.noreply.github.com>
2024-05-24 15:22:26 +0300

    new treeview provider for calculated variables

--- src/client/variable_definition_provider.ts
+++ src/client/variable_definition_provider.ts
@@ -1,67 +1,75 @@
 import * as vscode from "vscode";
-import { VariableLocation } from "./definition_provider";
+import { Equations, Variable } from "./variable";
 
-export class VariableDefinitionProvider
-  implements vscode.TreeDataProvider<VariableDefinition>
-{
-  update(definedVariableDefinitions: VariableDefinition[]) {
-    this.definedVariableLocations = definedVariableDefinitions;
-    this.refresh();
+export class VariableTreeItem extends vscode.TreeItem {
+  constructor(
+    public readonly variable: Variable, // Assuming Variable is the type of your variables
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState
+  ) {
+    super(variable.name, collapsibleState);
+    this.description = variable.evaluation;
   }
-  private _onDidChangeTreeData: vscode.EventEmitter<
-    VariableDefinition | undefined | null | void
-  > = new vscode.EventEmitter<VariableDefinition | undefined | null | void>();
-  readonly onDidChangeTreeData: vscode.Event<
-    VariableDefinition | undefined | null | void
-  > = this._onDidChangeTreeData.event;
 
-  constructor(private definedVariableLocations: VariableDefinition[]) {}
+  command = {
+    command: "extension.jumpToVariableDefinition",
+    title: "Jump to Definition",
+    arguments: [this.variable],
+  };
+}
+
+export class VariableTreeDataProvider
+  implements vscode.TreeDataProvider<VariableTreeItem>
+{
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    VariableTreeItem | undefined
+  > = new vscode.EventEmitter<VariableTreeItem | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<VariableTreeItem | undefined> =
+    this._onDidChangeTreeData.event;
+
+  constructor(private equations: Equations) {}
 
   refresh(): void {
-    this._onDidChangeTreeData.fire();
+    this._onDidChangeTreeData.fire(
+      undefined // undefined means refresh everything
+    );
   }
 
-  getTreeItem(element: VariableDefinition): vscode.TreeItem {
+  updateEquations(equations: Equations): void {
+    this.equations = equations;
+    this.refresh();
+  }
+
+  getTreeItem(element: VariableTreeItem): vscode.TreeItem {
     return element;
   }
 
-  getChildren(element?: VariableDefinition): Thenable<VariableDefinition[]> {
+  getChildren(element?: VariableTreeItem): Thenable<VariableTreeItem[]> {
     if (element) {
       return Promise.resolve([]);
     } else {
-      return Promise.resolve(this.definedVariableLocations);
+      return Promise.resolve(
+        this.equations.variables.map(
+          (variable) =>
+            new VariableTreeItem(variable, vscode.TreeItemCollapsibleState.None)
+        )
+      );
     }
   }
 }
 
-export class VariableDefinition extends vscode.TreeItem {
-  constructor(
-    public readonly name: string,
-    public readonly line: number,
-    public readonly start: number,
-    public readonly end: number,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState
-  ) {
-    super(name, collapsibleState);
-    this.command = {
-      command: "extension.jumpToVariableDefinition",
-      title: "Jump to Definition",
-      arguments: [this],
-    };
-  }
-
-  contextValue = "variableDefinition";
-
-  // factory from VariableLocation
-  static fromVariableLocation(
-    variableLocation: VariableLocation
-  ): VariableDefinition {
-    return new VariableDefinition(
-      variableLocation.name,
-      variableLocation.line,
-      variableLocation.start,
-      variableLocation.end,
-      vscode.TreeItemCollapsibleState.None
-    );
-  }
+export function jumpToDefinition(uri: vscode.Uri, variable: Variable) {
+  // Use the VS Code API to navigate to the variable's location
+  const range = new vscode.Range(
+    new vscode.Position(
+      variable.location.start.line,
+      variable.location.start.column
+    ),
+    new vscode.Position(
+      variable.location.end.line,
+      variable.location.end.column
+    )
+  );
+  vscode.window.showTextDocument(uri).then((editor) => {
+    editor.selection = new vscode.Selection(range.start, range.end);
+  });
 }
